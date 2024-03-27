daac8f1069f90b3c50040d8e5b46f6af3cb39fef
Pablo003 <129628068+Pablo088@users.noreply.github.com>
2024-03-27 11:10:39 -0300

    funciones.js

--- juego/funciones.js
+++ juego/funciones.js
@@ -1,7 +1,8 @@
 let contenedorDiv = document.getElementById("contenedor");
-let jugador = document.getElementById("decisionJugador");
 let verificar = document.getElementById("verificar");
+let jugador = document.getElementById("decisionJugador");
 let arrayNombres = [];
+let arrayComprobar = [];
 
 fetch("https://jsonplaceholder.typicode.com/users")
 .then(response => response.json())
@@ -10,24 +11,29 @@ fetch("https://jsonplaceholder.typicode.com/users")
 
 const traerData = (data) => {
     data.forEach(nombres => {
-            console.log(nombres.name);
-            
+            arrayComprobar.push(nombres.name);
+
             let buttons = document.createElement("button");
             buttons.type = "submit";
-            buttons.onclick = pasar();
             buttons.value = nombres.name;
-            buttons.innerHTML = `${nombres.name}`;
-            contenedorDiv.appendChild(buttons);
-
-            function pasar(){
-                jugador.appendChild(buttons);
-                arrayNombres = buttons.value;
-                let arrayComprobar = [arrayNombres];
-                arrayComprobar.sort();
-            }
-          
+            buttons.innerHTML = nombres.name;
+            contenedorDiv.append(buttons);  
             
-            buttons.addEventListener("click",pasar);
-           
+            buttons.addEventListener("click",()=>{
+                let botonesClic = buttons.cloneNode(true);
+                jugador.append(botonesClic);
+                arrayNombres.push(buttons.value);
+            })
     });
+    arrayComprobar.sort();
+    console.log(arrayComprobar);
+} 
+
+function comparar(){
+    if(arrayNombres.toString() == arrayComprobar.toString()){
+        verificar.innerHTML = 'Los nombres fueron ordenados alfabeticamente ¡Felicidades!';            
+    } else {
+        verificar.innerHTML = 'Los nombres no fueron ordenados alfabeticamente ¡Intentalo de nuevo que vos podes!';
+    }
+    console.log(arrayNombres);
 }
